cce4809e6cf7d5972d4b0f63ab5997fd25fc08ed
cutemonster <ghostshow@yandex.ru>
2020-02-18 13:31:12 +0200

    Little Changes

--- src/FileReading/FileRead.py
+++ src/FileReading/FileRead.py
@@ -11,20 +11,20 @@ if __name__ == "__main__":
         data = file.readlines()
     sorted_file = open("../ResultTableFiles/SortedFiles/SortedBySalary.txt", "w")
 
-    file_array = Helpful.parse_data_to_array_objects(data, File)
-    file_array = Helpful.sorted_table_file(sorted_file, file_array)
+    unsorted_file_array = Helpful.parse_data_to_array_objects(data, File)
+    sorted_file_array = Helpful.sorted_table_file(sorted_file, unsorted_file_array)
 
     search_value = int(input("Enter Amount of Salary to Search --> "))
 
-    Fibonacci_found_value = Fibonacci_Search.FibonacciSearch(file_array, search_value)
-    Helpful.printing_output("Fibonacci", file_array, Fibonacci_found_value, search_value)
+    Fibonacci_found_value = Fibonacci_Search.FibonacciSearch(sorted_file_array, search_value)
+    Helpful.printing_output("Fibonacci", sorted_file_array, Fibonacci_found_value, search_value)
 
-    Secventional_found_value = Secventional_Search.Secventional_Search(file_array, search_value)
-    Helpful.printing_output("Secventional", file_array, Secventional_found_value, search_value)
+    Secventional_found_value = Secventional_Search.Secventional_Search(sorted_file_array, search_value)
+    Helpful.printing_output("Secventional", sorted_file_array, Secventional_found_value, search_value)
 
-    Binary_found_value = Binary.binary_search(file_array, search_value)
-    Helpful.printing_output("Binary", file_array, Binary_found_value, search_value)
+    Binary_found_value = Binary.binary_search(sorted_file_array, search_value)
+    Helpful.printing_output("Binary", sorted_file_array, Binary_found_value, search_value)
 
-    Interpolation_found_value = Interpolation.interpolation_search(file_array, search_value)
-    Helpful.printing_output("interpolation", file_array, Interpolation_found_value, search_value)
+    Interpolation_found_value = Interpolation.interpolation_search(sorted_file_array, search_value)
+    Helpful.printing_output("interpolation", sorted_file_array, Interpolation_found_value, search_value)
 
